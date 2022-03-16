c0398ef0630a9ce1c9334874b57b027841b6b98d
Denikozub <denikozub@gmail.com>
2022-03-16 19:00:23 +0300

    add gloss signs

--- signatures.hs
+++ signatures.hs
@@ -1,30 +1,61 @@
+import Graphics.Gloss.Interface.Pure.Game
+
+-- Cell state
 data State = Alive | Dead
 
+-- Board - array of cells of set size
 data Board = Board {
   size :: Int,
   cells :: [State]
 }
 
+-- Errors while parsing
 data Error = ConfigurationError String | SizeError String
 
+-- Display mode
+display :: Display
+display = FullScreen
+
+-- Background colour
+bgColor :: Color
+bgColor = black
+
+-- Update frequency
+fps :: Int
+fps = 1
+
+-- Set board configuration from text
 setBoard :: String -> Either Error Board
 setBoard configuration -> Left (ConfigurationError "Wrong configuration")
 
+-- Get cell state by its position in array
 getState :: Int -> State
 getState pos = Dead
 
+-- Get list if cell neighbours by its position in array and array size
 neighbours :: Int -> Int -> [Int]
 neighbours size pos = []
 
+-- Update cell state by its current state and neighbours' states
 updateCell :: State -> Int -> State
 updateCell state aliveNeighbours = state
 
-updateBoard :: Board -> Board
-updateBoard board -> board
+-- Update all cells on board
+updateBoard :: Float -> Board -> Board
+updateBoard _ board -> board
+
+-- Draw a picture of current board state
+drawApp :: Board -> Picture
+drawApp board = Blank
+
+-- Handle IO events
+handleEvent :: Event -> AppState -> AppState
+handleEvent _ state = state
 
 main :: IO ()
   boardConfFile <- readLine
   configuration <- readFile boardConfFile
   case setBoard configuration of
     Left (ConfigurationError message) -> putStrLn message
-    Right board -> 
+    Right board -> do
+      play display bgColor fps board drawApp handleEvent updateBoard
