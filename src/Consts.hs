0f3d4f2e88856ffb766a7848fa2835d78c4c5d92
Denikozub <denikozub@gmail.com>
2022-04-01 16:25:15 +0300

    refactor

--- src/Consts.hs
+++ src/Consts.hs
@@ -14,3 +14,9 @@ aliveColor = white
 fps, screenSize :: Int
 fps = 1
 screenSize = 1000
+
+dimErrorMsg, dimRestrMsg, lineCountMsg, lineStateMsg :: String
+dimErrorMsg = "Size required on line 1"
+dimRestrMsg = "Size must be >= 3"
+lineCountMsg = "First line should be followed up with size^2 lines"
+lineStateMsg = "Each line after #1 should just contain 0 or 1"
